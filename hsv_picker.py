528229f78f7c94e89e59d4274141522587fbecb6
julio cesar <julio.jcgc@gmail.com>
2021-02-17 12:27:35 -0500

    minor changes

--- hsv_picker.py
+++ hsv_picker.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
-
 import os
 import sys
 
-ROOT_FOLDER = os.path.join(os.path.dirname(sys.path[0]), 'images')
-IMG_FOLDER = os.path.join(ROOT_FOLDER, 'masks_scaled')
+# ROOT_FOLDER = os.path.join(os.path.dirname(sys.path[0]), 'images')
+ROOT_FOLDER = sys.argv[1]
+IMG_FOLDER = os.path.join(ROOT_FOLDER, 'frames')
 
 image_hsv = None
 pixel = (20,60,80)
@@ -20,25 +20,34 @@ def pick_color(event,x,y,flags,param):
         print(pixel, lower, upper)
 
         image_mask = cv2.inRange(image_hsv,lower,upper)
+        cv2.namedWindow("mask", cv2.WINDOW_NORMAL)
         cv2.imshow("mask", image_mask)
 
 def main():
     import sys
     global image_hsv, pixel
 
-    img_name = os.path.join(IMG_FOLDER, 'img.png')
-
+    img_name = os.path.join(IMG_FOLDER, 'test.jpg')
+    print('Read image:', img_name)
+    
     image_src = cv2.imread(img_name)
     if image_src is None:
         print ("the image read is None............")
         return
+    cv2.namedWindow("bgr", cv2.WINDOW_NORMAL)
     cv2.imshow("bgr", image_src)
 
-    cv2.namedWindow('hsv')
+    cv2.namedWindow('hsv', cv2.WINDOW_NORMAL)
     cv2.setMouseCallback('hsv', pick_color)
 
     image_hsv = cv2.cvtColor(image_src,cv2.COLOR_BGR2HSV)
+    cv2.namedWindow("hsv", cv2.WINDOW_NORMAL)
     cv2.imshow("hsv",image_hsv)
+
+    save = False
+    if save:
+        cv2.imwrite(os.path.join(ROOT_FOLDER, 'hsv.jpg'), image_hsv)
+        cv2.imwrite(os.path.join(ROOT_FOLDER, 'bgr.jpg'), image_src)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
--- video_to_frames.py
+++ video_to_frames.py
@@ -1,12 +1,12 @@
 import cv2
 import os
+import sys
 
-root_path = '/home/msdc/jcgarciaca/projects/drone/process_video'
+root_path = sys.argv[1]
 videos_folder = os.path.join(root_path, 'videos')
-
 target_folder = os.path.join(root_path, 'frames')
 
-video_name = 'GX016809'
+video_name = 'DJI_0289_alto'
 video_path = os.path.join(videos_folder, video_name + '.MP4')
 
 if not os.path.exists(os.path.join(target_folder, video_name)):
@@ -16,16 +16,14 @@ cap = cv2.VideoCapture(video_path)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 print('length:', length)
 
-
 if (cap.isOpened()== False):
 	print("Error opening video stream or file")
-counter = 0
+counter = 1
 while(cap.isOpened()):
 	ret, frame = cap.read()	
 	if ret == True:
-		print('Processing frame #: {}'.format(counter))
+		print('Processing frame {}/{}'.format(counter + 1, length))
 		cv2.imwrite(os.path.join(target_folder, video_name, 'img_' + str(counter) + '.jpg'), frame)
 		counter += 1
-	if counter >= length - 1:#else:
-		break
-
+	if counter > length:
+		break	
